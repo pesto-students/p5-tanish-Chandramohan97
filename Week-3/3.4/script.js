758dbe3579b8b931d932a0052360575de6c72d85
Chandramohan97 <chandramsriv@gmail.com>
2022-09-04 23:06:54 +0530

    Feat : Implementing Encapsulation in Javascript.

---
+++ Week-3/3.4/script.js
@@ -0,0 +1,22 @@
+// Script File 
+
+function createStack() {
+    items = [];                      // declaring items in the function with let keyword so it's not accessible 
+    return {                         // outside of the createStack function
+      push(item) {
+        // var items = [];
+        items.push(item);
+      },
+      pop() {
+        items.pop();
+      },
+    };
+  }
+  
+  const stack = createStack();
+  stack.push(10);
+  // console.log(stack.items);
+  stack.push(5);
+  // console.log(stack.items);
+  stack.pop();
+  console.log(stack.items);
