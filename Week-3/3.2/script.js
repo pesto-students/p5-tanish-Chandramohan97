8bb6f0c02fe5c3319515543a83ddd097ec2e4508
Chandramohan97 <chandramsriv@gmail.com>
2022-09-04 19:38:43 +0530

    Documentation - brief description of Bind , Call and Appply method.

---
+++ Week-3/3.2/script.js
@@ -0,0 +1,11 @@
+// Working of Call , Bind and Apply. 
+// Bind, Call and Apply sort of work on the same principle i.e. to manipulate the function they're invoking with the
+// values provided through the method call. 
+// The  difference lies in the way the arguments is/are provided and the invokation process. 
+// With Call and Apply, the function can be invoked immediately whereas Bind returns a function - which has to invoked separately.
+
+// Code Below -
+
+
+
+
