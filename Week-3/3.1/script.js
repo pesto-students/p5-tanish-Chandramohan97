8cf035261a44c223657251665c045673c5fe62e3
Chandramohan97 <chandramsriv@gmail.com>
2022-09-05 00:49:43 +0530

    Feat - Adding a memoize function declaration.

--- Week-3/3.1/script.js
+++ Week-3/3.1/script.js
@@ -7,3 +7,26 @@ add = (...args) => {                              // Taking in unspecified numbe
     }
     return sum;
   }
+
+  const memoize = (add) => {                                              
+    let cache = {};
+    return function(...args) {
+    
+    // Using the map function to convert each element of the ...args array to String and concatenating thereafter using the join('') to create a singular element. Using this singular element as an array element afterwards and storing that in the cache variable.
+    
+    let cacheKey = args.map(n => n.toString() + '+').join('');
+    
+    if (cacheKey in cache) {
+        console.log("Already present in Cache");
+        return cache[cacheKey];
+      }
+      
+      else {
+        console.log('Calculating first time');
+        let result = add(...args);
+        cache[cacheKey] = result;
+        return result;
+      }
+    }
+  }
+  
