30e47dab58b1c0ac2446c8240eea10d9d04dd1eb
Chandramohan97 <chandramsriv@gmail.com>
2022-09-10 19:20:35 +0530

    Feat : Adding code to the script

--- Week-4/4.2/script.js
+++ Week-4/4.2/script.js
@@ -1,1 +1,12 @@
 // Showcasing Inheritance and Prototype in Javascript with usage of classes( The ES5 way)
+
+let Person = function(){};
+
+let Teacher = function(){}; 
+
+Person.prototype.initialise = function( name , age){   
+    this.name = name; 
+    this.age  = age;
+}
+
+Object.setPrototypeOf( Teacher.prototype , Person.prototype);    // Changing [[Protoype]] of Teacher.prototype to Person.protoype
