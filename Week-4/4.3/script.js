09f55f77c0ab28b05e97902a74a35e1a770b5ec0
Chandramohan97 <chandramsriv@gmail.com>
2022-09-10 14:01:44 +0530

    Feat : Adding the layout of the iterable object. Includes the Symbol.Iterator and next function

---
+++ Week-4/4.3/script.js
@@ -0,0 +1,11 @@
+// Implementing Fibonacci series with Iterators. 
+
+let Fib = { 
+    [Symbol.iterator](){
+        return{
+            next : function(){
+                
+            }
+        }
+    }
+}
