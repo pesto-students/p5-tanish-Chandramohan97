f2d22d6b5ac3809cb15869c8693197296205abab
Chandramohan97 <chandramsriv@gmail.com>
2022-09-10 14:04:52 +0530

    Feat + Docs : declaring and initialising the variables for Fibonacci implementation

--- Week-4/4.3/script.js
+++ Week-4/4.3/script.js
@@ -2,9 +2,18 @@
 
 let Fib = { 
     [Symbol.iterator](){
+        let term1 = 0, term2 = 1, term3; 
+        i = 3;
+        n= 8                        // n here is set to 8. User can change the value as per need.
+
+        console.log(term1);         
+        console.log(term2);
+
+        // Printing the first two terms of Fibonacci series as they are alwasy constant.
+
         return{
             next : function(){
-                
+
             }
         }
     }
