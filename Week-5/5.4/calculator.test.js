88d894196fc95c4efc65667bdd67eeef2d215bb8
Chandramohan97 <chandramsriv@gmail.com>
2022-09-17 23:07:55 +0530

    DOCS : Adding BDD test cases in test file

--- Week-5/5.4/calculator.test.js
+++ Week-5/5.4/calculator.test.js
@@ -1,17 +1,17 @@
+
 const mathsOperations = require('./calculator.js')  // importing mathsOperations object from calculator.js moduel//
 
-//Test case for Adding //
-test('Addition of 2 numbers', () =>{
-    expect(mathsOperations.sum(10,20)).toBe(30)    
-})
 
-//Test case for Subtraction //
-test('Difference of 2 numbers', () =>{
-    expect(mathsOperations.diff(10,20)).toBe(-10)
-})
+test('Addition of 2 numbers', () =>{                    // GIVEN when the user invoke sum function
+    expect(mathsOperations.sum(10,20)).toBe(30)        // WHEN he/she passes 2 numeric arguments
+})                                                    //  THEN the test runs successfully.
 
-//Test case for Multiplication //
 
-test('Multiplication of 2 numbers', () =>{
-    expect(mathsOperations.product(10,20)).toBe(200)
-})
+test('Difference of 2 numbers', () =>{                // GIVEN when the user invoke difference function
+    expect(mathsOperations.diff(10,20)).toBe(-10)    // WHEN he/she passes 2 numeric arguments
+})                                                  //  THEN the test runs successfully 
+
+
+test('Multiplication of 2 numbers', () =>{              // GIVEN when the user invoke product function
+    expect(mathsOperations.product(10,20)).toBe(200)    // WHEN he passes 2 numeric arguments  
+})                                                      // THEN the test runs successfully
