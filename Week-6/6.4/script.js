6da90fee1110e2e6e424737d1bfedea799f63834
Chandramohan97 <chandramsriv@gmail.com>
2022-09-22 12:19:21 +0530

    Feat : Code implementation for 6.4

---
+++ Week-6/6.4/script.js
@@ -0,0 +1,33 @@
+// Implementing code for Question 6.4 - Best Time to buy and sell stock//
+
+let arr = [7, 6, 4, 3, 1]
+let min = arr[0], max = arr[0], min_index = 0, max_index = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (min > arr[i]) {
+    min = arr[i];
+    min_index = i;
+  }
+
+  if (max < arr[i]) {
+    max = arr[i];
+    max_index = i;
+  }
+}
+
+if (max_index == 0) {                        // Incase the first element is of the maximum value.
+  max = arr[1];
+  for (let i = 2; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+      max_index = i;
+    }
+  }
+}
+
+if (max - min > 0 && min_index < max_index) {
+  console.log(`Profit is ${max - min}`);
+}
+else {
+  console.log('Profit is 0')
+}
