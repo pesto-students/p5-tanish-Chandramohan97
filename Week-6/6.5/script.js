4e62451666ff01c2c65be9c06a656a8dcdec1f68
Chandramohan97 <chandramsriv@gmail.com>
2022-09-24 20:11:30 +0530

    DOCS : Time and space complexity explanation

--- Week-6/6.5/script.js
+++ Week-6/6.5/script.js
@@ -1,6 +1,9 @@
 // Implementation to Pair With Given Difference //
 
 function returnTarget(arr, target) {
+
+    // Sorting the array before implementing Two pointer technique
+
     for (let i = 0; i < arr.length - 1; i++) {
       if (arr[i] > arr[i + 1]) {
         let temp = arr[i];
@@ -20,3 +23,7 @@ function returnTarget(arr, target) {
     return ctr;
   }
   console.log(returnTarget(arr, 78));
+
+  // Space Complexity : O(1) - as no new array was created during the operation. 
+  // Time Complexity  : O(N). The time complexity is (3N)/2 - 'N' for the for loop starting from line 7(till line 14)
+  // and N/2 for the while loop starting from line 18( TILL 24)
