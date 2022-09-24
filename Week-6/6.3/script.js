a4d93ce2cd01f303fde420976958f30d83b04754
Chandramohan97 <chandramsriv@gmail.com>
2022-09-24 17:41:31 +0530

    refactor : changing the code as the former code wasn't satisfying all the criteria.

--- Week-6/6.3/script.js
+++ Week-6/6.3/script.js
@@ -1,21 +1,29 @@
-arr = [0, 2,1,1,1,1,1,0]
+arr = [2,2,2,2,2,2,0,1,2,1,0,1,2]
+let ctr0 = 0, ctr1 = 0 , ctr2 = 0;
 
 for (let i = 0; i < arr.length - 1; i++) {
-  if (arr[i] > arr[i + 1]) {
-    k = arr[i];
-    arr[i] = arr[i + 1];
-    arr[i + 1] = k;
+  if( arr[i] == 0){ ctr0 ++;}
+  else if( arr[i] == 1){ ctr1 ++;}
+  else( arr[i] == 2)
+    { ctr2 ++;}
+}
+let i = 0; 
+while( i < arr.length){
+  if( ctr0 != 0 ){
+    arr[i] = 0; 
+    ctr0 -= 1; 
+    i++;
+  }else if( ctr1!= 0){
+    arr[i] = 1; 
+    ctr1 -= 1; 
+    i++;
+  }else{
+     arr[i] = 2; 
+     ctr2 -= 1; 
+     i++;
+    
   }
 }
-
-for (let i = arr.length - 1; i > 0; i--) {
-  if (arr[i] < arr[i - 1]) {
-    k = arr[i];
-    arr[i] = arr[i - 1];
-    arr[i - 1] = k;
-  }
-}
-
 console.log(arr);
 
 // Time Complexity = O(n) - as the for loop runs twice for Nth time i.e. N+N = 2N ~ N = O(N)
