3d40da129cd6762b093771ab98665a16031524a4
Chandramohan97 <chandramsriv@gmail.com>
2022-10-09 04:06:44 +0530

    feat : implementing the driving code

--- Week-7/7.4/script.js
+++ Week-7/7.4/script.js
@@ -20,3 +20,26 @@ class Stack{
         return this.items[this.items.length - 1];   // to access last element
     }
 }
+
+function ParenthesisChecker(exp){
+    
+    let stack = new Stack(); 
+    
+    for(let i = 0 ; i < exp.length ; i++){
+        if(stack.length() == 0 || exp[i] == '{' || exp[i] == '(' || exp[i] =='['){
+            stack.push(exp[i]);
+        }
+        
+        else if( ( stack.lastElement() == '[' && exp[i] == ']')
+            ||  ( stack.lastElement() == '{' && exp[i] == '}' )
+            ||  ( stack.lastElement() == '(' && exp[i] == ')' ) ){
+                stack.pop();
+            }
+        else{
+            stack.push(exp[i]);
+        }
+    
+    }
+    if(stack.length() == 0 ) return true ; 
+    else{ return false;}
+}
