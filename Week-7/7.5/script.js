2bcd5dddacff27111b0a6d614b99de5a3fc43e2c
Chandramohan97 <chandramsriv@gmail.com>
2022-10-08 23:09:38 +0530

    feat : Creating Stack class

--- Week-7/7.5/script.js
+++ Week-7/7.5/script.js
@@ -1,2 +1,16 @@
 // Implementing JS code to find the time and space complexity pertaining to finding the next 
 // Greater element
+
+class Stack{
+    constructor(items){
+        this.items = [];
+    }
+    push(element){
+        return this.items.push(element);
+    }
+    
+    pop(){
+        return this.items.pop();
+    }
+    
+}
