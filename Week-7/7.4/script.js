471a4de2ce1f1938fee15ac599b11ebf67f3d8fe
Chandramohan97 <chandramsriv@gmail.com>
2022-10-09 04:04:51 +0530

    feat : class Stack introduced

--- Week-7/7.4/script.js
+++ Week-7/7.4/script.js
@@ -1,1 +1,22 @@
 //Implement to perform Javascript Parenthesis Checks through Stacks 
+
+class Stack{
+    constructor(items){
+        this.items = [];
+    }
+    push(element){
+        return this.items.push(element);
+    }
+    
+    pop(){
+        return this.items.pop();
+    }
+    
+    length(){
+        return this.items.length;
+    }
+    
+    lastElement(){
+        return this.items[this.items.length - 1];   // to access last element
+    }
+}
