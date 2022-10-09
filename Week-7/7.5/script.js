5558b7a49c779e2dfef3b567ebae047dc720272a
Chandramohan97 <chandramsriv@gmail.com>
2022-10-09 12:17:45 +0530

    feat : Main code.

--- Week-7/7.5/script.js
+++ Week-7/7.5/script.js
@@ -12,5 +12,35 @@ class Stack{
     pop(){
         return this.items.pop();
     }
-    
+
+    length(){
+        return this.items.length;
+    }
+
+    accessLastElement(){
+        return this.items[this.items.length - 1];
+    }    
 }
+ 
+function lastGreater(arr){
+    let stack = new Stack();
+    stack.push(arr[0]);
+    for( i = 1; i < arr.length ; i++){
+        
+        while( stack.length() !== 0 && stack.accessLast() < arr[i]){
+            console.log(stack.accessLast() + "-->" + arr[i]);
+            stack.pop();
+    }
+        stack.push(arr[i]);
+    }
+    
+    while(stack.length() !== 0){
+        // let lastElement = stack.accessLastElement(); 
+        console.log( stack.items[stack.length() - 1] + "--›" + -1);
+        stack.pop();
+    }
+    
+    return 0;
+}
+
+lastGreater([1,3,2,4]);
