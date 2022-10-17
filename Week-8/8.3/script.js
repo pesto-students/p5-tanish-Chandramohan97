cc885f4443bbc8cf2bd095e8042ca484eff92423
Chandramohan97 <chandramsriv@gmail.com>
2022-10-17 12:54:45 +0530

    feat : main code to implement level order traversal.

--- Week-8/8.3/script.js
+++ Week-8/8.3/script.js
@@ -1,1 +1,32 @@
 // JS code to implement level order traversal 
+
+class Node{
+    constructor(value){
+        this.data = value; 
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function levelOrder(root){
+    let queue = [];
+    queue.push(root);
+    // console.log(queue);
+    
+    while(queue.length !== 0){
+        let node = queue.shift();
+        console.log(node.data);
+        
+        if(node.left)
+            queue.push(node.left);
+        if(node.right)
+            queue.push(node.right)
+    }
+}
+
+root = new Node(3);
+root.left = new Node(9);
+root.right = new Node(20);
+root.left.left = new Node(15);
+root.left.right = new Node(7);
+levelOrder(root);
