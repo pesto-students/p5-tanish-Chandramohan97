af4f21e7c388ea4da25cfdaef3799a7085a9149f
Chandramohan97 <chandramsriv@gmail.com>
2022-10-21 20:24:51 +0530

    feat : JS code for 8.2

---
+++ Week-8/8.2/script.js
@@ -0,0 +1,35 @@
+class Tree{
+    constructor(data){
+        this.value = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+function isValidBST(node, min, max) {
+  if (node === null) {
+    return true;
+  }
+  
+  if (
+    (max !== null && node.value > max) ||
+    (min !== null && node.value < min)
+  ) {
+    return false;
+  }
+  
+  if (
+    !isValidBST(node.left, min, node.value) ||
+    !isValidBST(node.right, node.value, max)
+  ) {
+    return false;
+  }
+  return true;
+}
+
+root = new Tree(4);
+root.left = new Tree(2);
+root.right = new Tree(5);
+root.left.left = new Tree(1);
+root.left.right = new Tree(3)
+
+console.log("BST is", isValidBST(root, Number.MIN_VALUE, Number.MAX_VALUE));
