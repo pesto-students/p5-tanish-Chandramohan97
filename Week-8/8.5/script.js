c36ac6299ea0cc6645f33b36ebeb219c2430f05f
Chandramohan97 <chandramsriv@gmail.com>
2022-10-21 18:28:40 +0530

    feat : JS code for 8.5

---
+++ Week-8/8.5/script.js
@@ -0,0 +1,33 @@
+function findJudge(N,trust){
+    
+    const likesList ={}; //tracking how many likes an element gives
+    const beingLikedList = {};  //tracking how many likes an element receives
+    let judge = 0;
+    
+    for( i = 1; i <= N ; i++){
+        likesList[i] = 0; 
+        beingLikedList[i] = 0;
+    }
+    
+    for(ele of trust){
+        likesList[ele[0]]++;
+        beingLikedList[ele[1]]++;
+    }
+    
+    for(key in likesList){
+        if( likesList[key] == 0){
+            judge = key;
+            break;
+        }
+    }
+    
+    if( beingLikedList[judge] == N - 1)
+        return judge;
+    else
+        return -1;
+}
+
+// trust = [[1,3] , [2,3] , [3,1]]
+trust = [[1,3], [2,3]]
+// console.log(findJudge([1,3],[2,3],[3,1]));
+console.log(findJudge(3,trust));   // N = Number of people.
