1c210ad58f50df299e3ca53ba92caed323771b5c
Chandramohan97 <chandramsriv@gmail.com>
2022-11-09 11:23:53 +0530

    feat : introducing Header component

---
+++ Week-9/todolist/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <div className = 'header'>
+            <h2> To Do List</h2>
+        </div>
+    )
+}
+
+export default Header;
