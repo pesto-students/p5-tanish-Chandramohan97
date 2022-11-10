0c147226a8055c328083f9b719a11131b51e6258
Chandramohan97 <chandramsriv@gmail.com>
2022-11-10 11:43:58 +0530

    feat : adding the helper function to store the list in an array.

--- Week-9/todolist/src/components/ToDoForm.js
+++ Week-9/todolist/src/components/ToDoForm.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './toDoForm.css'
 
 function ToDoForm(){
+  const [list,setList] = useState({});
+  const [items, setItems] = useState([]);
+  
+  function addItem(){
+    
+    const item= {
+      id : Math.floor(Math.random() * 1000),
+      value : list
+    };
+
+    setItems( items => [...items,item]);
+    console.log(items);
+
+    setList("");
+  }
   return (
-    <form>
-        <input type ="text" /> 
-    </form>
+    // replacing form with <div> container as the console.log would disappear immediately post submit.
+     <div className= "form">                
+        <input 
+        type ="text"
+        size = '50' 
+        onChange ={e => setList(e.target.value)}
+        /> 
+        <button onClick={addItem}> Add </button>
+      </div>
   )
 }
 
