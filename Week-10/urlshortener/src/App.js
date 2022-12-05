44ef870eb287d46367713fb185776fdc8722a9de
Chandramohan97 <chandramsriv@gmail.com>
2022-12-05 10:15:14 +0530

    including Footer component in the App.js.

--- Week-10/urlshortener/src/App.js
+++ Week-10/urlshortener/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import InputContainer from './Components/InputContainer.js';
 import Navbar from './Components/Navbar.js';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <>
       <Navbar/>
       <InputContainer />
+      <Footer />
     </>
     );
 }
