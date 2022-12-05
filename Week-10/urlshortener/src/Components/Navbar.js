340f4528549fbb7784bb2d9bdf59cbed1002a2dc
Chandramohan97 <chandramsriv@gmail.com>
2022-12-05 08:46:18 +0530

    feat : js file for navigation bar.

---
+++ Week-10/urlshortener/src/Components/Navbar.js
@@ -0,0 +1,26 @@
+// Header component for URL Shortener
+import React from 'react';
+import './Navbar.css'
+
+function Navbar(){
+    return(
+        <nav>
+            <h3 class="logo"> Shortly</h3>
+            <ul className='left-items'>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Pricing</a></li>
+                <li><a href="#">Resources</a></li>
+            </ul>
+
+            <ul className='right-items'>
+
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Sign up</a></li>
+        
+             </ul>
+
+        </nav>
+    );
+}
+
+export default Navbar;
