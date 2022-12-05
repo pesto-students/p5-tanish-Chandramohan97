8033aea880dd9b0d1b0c1c841428de617b70213e
Chandramohan97 <chandramsriv@gmail.com>
2022-12-05 10:13:44 +0530

    feat : Footer component.

---
+++ Week-10/urlshortener/src/Components/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+            <h3> Shortly</h3>
+            <ul>
+                <li> Features</li>
+                <li> Link Shortening</li>
+                <li> Branded</li>
+            </ul>
+
+            <ul>
+                <li> Resources</li>
+                <li> Blog</li>
+                <li> Devlopers</li>
+            </ul>
+
+            <ul>
+                <li> Company</li>
+                <li> About</li>
+                <li> Our Team</li>
+            </ul>  
+
+            <ul>
+                <li> Login </li>
+                <li> Sign up</li>    
+            </ul>         
+    </footer>
+  )
+}
+
+export default Footer;
