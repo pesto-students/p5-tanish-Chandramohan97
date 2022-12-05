7a0f10c91d72b222918e3602576509810c1d7bdb
Chandramohan97 <chandramsriv@gmail.com>
2022-12-06 03:37:04 +0530

    feat : React implementation for Input Container.

--- Week-10/urlshortener/src/Components/InputContainer.js
+++ Week-10/urlshortener/src/Components/InputContainer.js
@@ -1,13 +1,56 @@
 // Component input Bar 
 
-import React from 'react';
-import './Components_styling/InputContainer.css'
+import React, { useState } from 'react';
+import axios from "axios";
+import './InputContainer.css'
+// import './Components_styling/InputContainer.css'
 
 function InputBar(){
+
+    const [input,setInput] = useState("");
+    const [url,setURL] = useState("");
+    const [urlList,setList] = useState([]);
+    const [loading,setLoading] = useState(false);
+
+    let baseUrl = `https://api.shrtco.de/v2/shorten?url=${input}`
+
+    const Input = (e) =>{
+        setInput(e.target.value);
+        // console.log(input);
+    }
+
+    const fetchURL = async(e) => {
+        
+        setLoading(true);
+        try{
+            
+            // console.log(baseUrl)
+            await axios(baseUrl).then((response) => {
+                setURL(response.data);
+                // console.log(response.data);
+                setList(urlList.push(response.data));
+                // console.log(response);
+                console.log(urlList);
+            })
+            
+        }catch(error){
+            console.log(baseUrl);
+            alert('Enter a valid URL')
+            // console.log(error);
+        }       
+        setLoading(false);
+    }
     return (
         <div className='inputBar'>
-            <input placeholder='Shorten the link here' />
-            <button> Shorten it</button>
+
+            <input 
+            type="text"
+            value={input}
+            placeholder='Shorten the link here'
+            onChange={Input}
+             />
+
+            <button onClick={fetchURL}>Shorten</button>
         </div>
     )    
 }
@@ -18,5 +61,6 @@ function InputContainer(){
     )
 
 }
+
 
 export default InputContainer;
