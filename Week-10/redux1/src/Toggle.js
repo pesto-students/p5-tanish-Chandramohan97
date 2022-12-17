36eabf9c036adcad0b905e70f666c6047d934d41
Chandramohan97 <chandramsriv@gmail.com>
2022-12-17 18:39:19 +0530

    incorporating useSelector and Dispatch in Toggle

--- Week-10/redux1/src/Toggle.js
+++ Week-10/redux1/src/Toggle.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Toggle.css";
+import { useSelector, useDispatch } from "react-redux";
+import { lightOn, lightOff } from "./redux/action";
 
 const Toggle = () => {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
   return (
     <div className={state ? "lit" : "dark"}>
       <h2> the room is {state ? "lit" : "dark"}</h2>
