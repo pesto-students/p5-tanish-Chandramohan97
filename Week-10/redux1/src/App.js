a35069a633e392c26956d62b07aaa1deb1298b87
Chandramohan97 <chandramsriv@gmail.com>
2022-12-17 18:38:48 +0530

    incorporating store in App.js

--- Week-10/redux1/src/App.js
+++ Week-10/redux1/src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 // import './App.css';
 import Toggle from "./Toggle";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 function App() {
   return (
     <div className="App">
