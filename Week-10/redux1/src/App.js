8aa982b50e644e18cabe874b323cb02006deed73
Chandramohan97 <chandramsriv@gmail.com>
2022-12-17 18:43:58 +0530

    styling from App.css

--- Week-10/redux1/src/App.js
+++ Week-10/redux1/src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
-// import './App.css';
+import "./App.css";
 import Toggle from "./Toggle";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 function App() {
   return (
-    <div className="App">
-      <Toggle />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Toggle />
+      </div>
+    </Provider>
   );
 }
 
