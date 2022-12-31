ac50eb292f8d38cb61efc8ba655cf0c8532d4728
Chandramohan97 <chandramsriv@gmail.com>
2022-12-31 22:54:16 +0530

    feat : adding url for multiple days( in app.js) feat : adding input form for days in index.html

--- Week-13/app.js
+++ Week-13/app.js
@@ -5,6 +5,9 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const port = 3000;
 
+// app.use(express.json());
+// app.use("/weather", userController);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -12,18 +15,37 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   const city = req.body.city;
+  const days = req.body.days;
   const apiKey = "89243cbe4845f1116a376e5092c245ad";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`; // API for one day.
+  const url2 = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=${days}&appid=${apiKey}`;
 
-  https.get(url, (response) => {
-    response.on("data", (data) => {
-      var weatherData = JSON.parse(data);
-      temp = weatherData.main.temp - 273;
-      //   res.send('Weather forecast for ' +c)
-      res.send("Temp for today for " + city + " is " + Math.ceil(temp) + "°C");
-      //   console.log(weatherData.main.temp - 273);
+  if (days == 1) {
+    https.get(url, (response) => {
+      response.on("data", (data) => {
+        var weatherData = JSON.parse(data);
+        temp = weatherData.main.temp - 273;
+        //   res.send('Weather forecast for ' +c)
+        res.send(
+          "Temp for today for " + city + " is " + Math.ceil(temp) + "°C"
+        );
+        //   console.log(weatherData.main.temp - 273);
+      });
     });
-  });
+  } else {
+    https.get(url2, (response) => {
+      response.on("data", (data) => {
+        var weatherData = JSON.parse(data);
+        // temp = weatherData.main.temp - 273;
+        //   res.send('Weather forecast for ' +c)
+        res.send(
+          weatherData
+          // "Temp for today for " + city + " is " + Math.ceil(temp) + "°C"
+        );
+        //   console.log(weatherData.main.temp - 273);
+      });
+    });
+  }
 });
 
 app.listen(port, () => {
