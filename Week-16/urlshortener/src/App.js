ff9ebf4a15844379c482d46714839a6a8b646bdd
Chandramohan97 <chandramsriv@gmail.com>
2023-01-13 22:14:52 +0530

    importing Sentry onto App and Index

--- Week-16/urlshortener/src/App.js
+++ Week-16/urlshortener/src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
-import InputContainer from './Components/InputContainer.js';
-import Navbar from './Components/Navbar.js';
-import Footer from './Components/Footer';
-import LinkResult from './Components/LinkResult';
-
+import "./App.css";
+import InputContainer from "./Components/InputContainer.js";
+import * as Sentry from "@sentry/react";
+import Navbar from "./Components/Navbar.js";
+import Footer from "./Components/Footer";
+import LinkResult from "./Components/LinkResult";
+import SentryComp from "./Components/SentryComp";
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <SentryComp />
+      <Navbar />
       <InputContainer />
       <Footer />
     </>
-    );
+  );
 }
 
-export default App;
+export default Sentry.withProfiler(App);
--- Week-16/urlshortener/src/index.js
+++ Week-16/urlshortener/src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//Add these lines
+Sentry.init({
+  dsn: "https://aca531443fbd4cb0bc8e2d00e80a75c0@o4504484017471488.ingest.sentry.io/4504487438188544",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
