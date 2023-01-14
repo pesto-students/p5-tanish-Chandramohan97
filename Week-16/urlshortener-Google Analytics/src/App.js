de317ba3ab0eb8bf41b85a306ae7c9d3ccaaf18f
Chandramohan97 <chandramsriv@gmail.com>
2023-01-14 10:12:36 +0530

    feat : Implementing ReactGA in app.js

--- Week-16/urlshortener-Google Analytics/src/App.js
+++ Week-16/urlshortener-Google Analytics/src/App.js
@@ -1,18 +1,24 @@
-import './App.css';
-import InputContainer from './Components/InputContainer.js';
-import Navbar from './Components/Navbar.js';
-import Footer from './Components/Footer';
-import LinkResult from './Components/LinkResult';
-
+import "./App.css";
+import React from "react";
+import InputContainer from "./Components/InputContainer.js";
+import Navbar from "./Components/Navbar.js";
+import Footer from "./Components/Footer";
+import LinkResult from "./Components/LinkResult";
+import ReactGA from "react-ga4";
+const TRACKING_ID = "UA-251171587-1";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  React.useEffect(() => {
+    ReactGA.pageview(window.location.pathname); // capturing the pageview i.e. recording the number of times user visit the page.
+  }, []);
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <InputContainer />
       <Footer />
     </>
-    );
+  );
 }
 
 export default App;
